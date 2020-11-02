769c328bbe18774956453ccbf1ab7425d7c53737
Ignot Zarudko <ascendp6n9@gmail.com>
2020-11-02 19:10:50 +0300

    add connectivity components search

--- Graph/Graph/Graph.cs
+++ Graph/Graph/Graph.cs
@@ -1,12 +1,82 @@
-﻿namespace Graph
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph
 {
     public class Graph
     {
-        public int[][] AdjacencyMatrix { get; set; }
+        private List<List<int>> AdjacencyLists { get; set; }
+        private List<List<int>> ConnectivityComponents { get; set; }
+        private List<bool> UsedVertexes { get; set; }
 
-        public Graph(int[][] adjacencyMatrix)
+        public Graph(List<List<int>> adjacencyLists)
         {
+            AdjacencyLists = adjacencyLists;
+            ConnectivityComponents = new List<List<int>>();
+            UsedVertexes = new List<bool>();
+            for (int i = 0; i < AdjacencyLists.Count; i++)
+            {
+                UsedVertexes.Add(false);
+            }
             
+            FindConnectivityComponents();
+        }
+
+        public void FindConnectivityComponents()
+        {
+            for (int i = 0; i < UsedVertexes.Count; i++)
+            {
+                if (!UsedVertexes[i])
+                {
+                    List<int> currentConnectivityComponent = new List<int>();
+                    FindOneComponent(i, currentConnectivityComponent);
+                    ConnectivityComponents.Add(currentConnectivityComponent);
+                }
+            }
+            
+            UsedVertexes.ForEach(vertex => vertex = false);
+        }
+
+        private void FindOneComponent(int currentVertex, List<int> currentConnectivityComponent)
+        {
+            UsedVertexes[currentVertex] = true;
+            currentConnectivityComponent.Add(currentVertex);
+
+            for (int i = 0; i < AdjacencyLists[currentVertex].Count; i++)
+            {
+                int whereDoWeGoNext = AdjacencyLists[currentVertex][i];
+                if (!UsedVertexes[whereDoWeGoNext])
+                {
+                    FindOneComponent(whereDoWeGoNext, currentConnectivityComponent);
+                }
+            }
+        }
+
+        public void PrintConnectivityComponents()
+        {
+            Console.WriteLine($"Connectivity Components. Total Amount is {ConnectivityComponents.Count}");
+            
+            for (int i = 0; i < ConnectivityComponents.Count; i++)
+            {
+                StringBuilder sb = new StringBuilder("");
+                
+                sb.Append($"Component {i} - {{ ");
+                foreach (int currentVertex in ConnectivityComponents[i])
+                    sb.Append($"{currentVertex}, ");
+
+                sb.Remove(sb.Length - 2, 1);
+                sb.Append("}");
+                Console.WriteLine(sb.ToString());
+            }
+        }
+
+        private void ClearUsedVertices()
+        {
+            for (int i = 0; i < AdjacencyLists.Count; i++)
+            {
+                UsedVertexes[i] = false;
+            }
         }
     }
 }
--- Graph/Graph/Program.cs
+++ Graph/Graph/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Graph
 {
@@ -6,7 +7,34 @@ namespace Graph
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vertexesArray1 = new[] {1, 2};
+            int[] vertexesArray2 = new[] {0, 2};
+            int[] vertexesArray3 = new[] {0, 1};
+            int[] vertexesArray4 = new[] {4, 5};
+            int[] vertexesArray5 = new[] {3};
+            int[] vertexesArray6 = new[] {3};
+            int[] vertexesArray7 = new[] {7, 8};
+            int[] vertexesArray8 = new[] {6};
+            int[] vertexesArray9 = new[] {6, 9};
+            int[] vertexesArray10 = new[] {8};
+            int[] vertexesArray11 = new int[0];
+            
+            List<List<int>> graphList = new List<List<int>>();
+            graphList.Add(new List<int>(vertexesArray1));
+            graphList.Add(new List<int>(vertexesArray2));
+            graphList.Add(new List<int>(vertexesArray3));
+            graphList.Add(new List<int>(vertexesArray4));
+            graphList.Add(new List<int>(vertexesArray5));
+            graphList.Add(new List<int>(vertexesArray6));
+            graphList.Add(new List<int>(vertexesArray7));
+            graphList.Add(new List<int>(vertexesArray8));
+            graphList.Add(new List<int>(vertexesArray9));
+            graphList.Add(new List<int>(vertexesArray10));
+            graphList.Add(new List<int>(vertexesArray11));
+            
+            Graph graph = new Graph(graphList);
+            
+            graph.PrintConnectivityComponents();
         }
     }
 }
